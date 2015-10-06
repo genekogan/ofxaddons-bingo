dcfcee702955ad572384ab8ae1f773981bc9d73d
Gene Kogan <kogan.gene@gmail.com>
2015-10-06 19:37:34 -0400

    updated readme and addon bash script

--- clone_addons.sh
+++ clone_addons.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ../../addons2
+cd ../../addons
 git clone https://github.com/maxillacult/ofxPostGlitch
 git clone https://github.com/motoishmz/ofxJapan
 git clone https://github.com/kylemcdonald/ofxCcv
@@ -9,3 +9,4 @@ git clone https://github.com/2bbb/ofxMacMouse
 git clone https://github.com/elaye/ofxMarkovChain
 git clone https://github.com/kylemcdonald/ofxFaceTracker.git
 git clone https://github.com/vanderlin/ofxBox2d.git
+git clone https://github.com/kylemcdonald/ofxCv.git
